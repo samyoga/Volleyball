a627986ed0fadbbcb583589f79a7bbffa0840826
Samyoga <beyondthehorizon@outlook.com>
2018-02-10 18:07:38 +0545

    Resize net and add volleyball

--- main.c
+++ main.c
@@ -5,28 +5,15 @@
   {
         /* request auto detection */
         int gd = DETECT, gm, err;
-        int radius = 10, x, y, midy,midx;
+        int radius = 10, x, y, midy,midx,i;
 
         /* initialize graphic mode */
         initgraph (&gd, &gm,NULL);
-      /*  err = graphresult(); */
-
-       /* if (err != grOk) {
-                /* error occurred */
-              /*  printf("Graphics Error: %s\n",
-                                grapherrormsg(err));
-                return 0;
-        } */
-
         x = 40;
         midx = getmaxx() /2;
         midy = getmaxy() / 2;
         y = midy + 100;
 
-        /*
-         * used 5 stick man (still/image)
-         * position to get walking animation
-         */
         while (x < getmaxx() - 25) {
                 /* clears graphic screen */
                 cleardevice();
@@ -35,7 +22,7 @@
                 /* road for stick man */
                 line(0, getmaxy() - 50, getmaxx(),getmaxy() -50);
 
-                /* image 1  - first position of 1st stick man */
+                /* image 1  -position of 1st stick man */
                 circle(x, y, radius);
                 line(x, y + radius, x, y + radius + 50);
 
@@ -59,14 +46,16 @@
                 line(x+550, y + radius + 10,x+ 535, y + radius + 40);
                 line(x+550, y + radius + 10,x+565, y + radius + 40);
 
+                for (i=0;i<10;i++){
                 /*net design*/
                 line(midx,getmaxy() - 50, midx, midy+30);
                 line(midx,midy+30,midx-50,midy-100);             
                 line(midx-50,midy-100,midx-50,midy+50);
                 line(midx,getmaxy()-100,midx-50,midy);
+                line(0,midy+50,getmaxx(),midy+50);}
 
-
-
+                /*ball design*/
+                circle(x+50,y+50,radius);
         }
 
         getch();
